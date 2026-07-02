3d6f0ba8eb24f33bafdf8753bc204559ff9ff77f
Job <Jobsjobbema@gmail.com>
2026-07-02 23:36:07 +0200

    fixes

--- gym-app/app.py
+++ gym-app/app.py
@@ -1,8 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
+
+db = SQLAlchemy()
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
+migrate = Migrate(app, db)
+
+
+from models import User
 
 
 @app.route("/")
 def home():
-    return "GymBros test"
+    return "GymBros werkt!"
